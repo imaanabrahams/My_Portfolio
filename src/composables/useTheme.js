8a24b1781305c11b353728398f1f555cacfc2a33
Imaan Abrahams <imaanabrahams15@gmail.com>
2026-09-22 08:54:56 +0200

    :art:

--- src/composables/useTheme.js
+++ src/composables/useTheme.js
@@ -5,7 +5,7 @@ const STORAGE_KEY = 'ia-theme'
 
 function apply(value) {
   document.documentElement.dataset.theme = value
-  document.querySelector('meta[name="theme-color"]')?.setAttribute('content', value === 'dark' ? '#ffb6c1' : '#ff8fab')
+  document.querySelector('meta[name="theme-color"]')?.setAttribute('content', value === 'dark' ? '#90ee90' : '#ff8fab')
 }
 
 export function useTheme() {
